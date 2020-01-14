e36ce4e06018c353843296b2f811b875639d2bdb
f-sartori <fmarcelo75@gmail.com>
2020-01-14 08:33:17 -0300

    Projeto criado

---
+++ src/main/java/com/generation/java_14_01/App.java
@@ -0,0 +1,13 @@
+package com.generation.java_14_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
